99e59385c7cf887a746c112f8b98364d55073df1
Muneer Kalakuzhiyil <muneerkk66@gmail.com>
2024-01-11 17:33:15 +0000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Muneer K K on 11/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
